7c4b45a59269fd5f6b2612af8e23e84953a10205
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-02-03 16:50:33 -0600

    Se trabajó la zona de About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import styles from './About.module.css';
+import st from './About.module.css';
 
 
 const About = (props) => {
 
+    return(
+        <section className={st.section}>
+            <img className={st.section_principalImg} src='./src/assets/cocineraTitulos.png'/>
+            <div className={st.section_divContent}>
+                <h2 className={st.divContent_h2}>Sobre nosotros</h2>
+                <p className={st.divContent_p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem id laborum similique a ipsum nisi eaque nemo molestias dolor perspiciatis corrupti porro impedit aliquid, distinctio quisquam sint nobis obcaecati blanditiis.</p>
+                <div className={st.divSubContent}>
+                    <p className={st.divSubContent_p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem id laborum similique a ipsum nisi eaque nemo molestias dolor perspiciatis corrupti porro is.</p>
+                    <div className={st.divSubContent_img}><img src='./src/assets/porcion de torta.png'/></div>
+                </div>
+            </div>
+        </section>
+    )
 
 }
 
